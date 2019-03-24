848db4829c0b5dcb4d7481cbe7f68db45a153062
Rhys Xia <xrs4433@outlook.com>
2019-03-24 21:54:05 +0800

    :sparkles:初步完成基于react的markdown的加载器

--- index.js
+++ index.js
@@ -1,14 +1,47 @@
 const frontMatter = require('front-matter')
 const highlight = require('highlight.js')
 const MarkdownIt = require('markdown-it')
+const utils = require('loader-utils')
 
 
 const DEFAULT_MARKDOWN_OPTIONS = {
+    linkify: true,
+    typographer: true,
+    xhtmlOut: true,
+    highlight(content, languageHint) {
+        let highlightedContent;
 
+        highlight.configure({
+            useBR: true,
+            tabReplace: '    '
+        });
+
+        if (languageHint && highlight.getLanguage(languageHint)) {
+            try {
+                highlightedContent = highlight.highlight(languageHint, content).value;
+            } catch (err) {
+            }
+        }
+
+        if (!highlightedContent) {
+            try {
+                highlightedContent = highlight.highlightAuto(content).value;
+            } catch (err) {
+            }
+        }
+
+        // 把代码中的{}转
+        highlightedContent = highlightedContent.replace(/[\{\}]/g, (match) => `{'${match}'}`);
+
+        // 加上 hljs
+        highlightedContent = highlightedContent.replace('<code class="', '<code class="hljs ').replace('<code>', '<code class="hljs">')
+
+        return highlight.fixMarkup(highlightedContent);
+    }
 }
 
 const defaultOptions = {
-    imports: `import React from 'react'`,
+    imports: `import React from 'react'\nimport 'highlight.js/styles/github.css'`,
     mdOptions: Object.assign({}, DEFAULT_MARKDOWN_OPTIONS),
     plugins: [],
     rules: {},
@@ -35,9 +68,8 @@ function renderModule(imports, jsx) {
 module.exports = function (source) {
     this.cacheable()
 
-    const { body, attributes: { imports: importMap } } = frontMatter(source)
-    const { imports, mdOptions, plugins, preProcess, postProcess, rules } = Object.assign({}, defaultOptions, this.options)
-
+    const { imports, mdOptions, plugins, preProcess, postProcess, rules } = Object.assign({}, defaultOptions, utils.getOptions(this))
+    
     const md = new MarkdownIt(mdOptions)
 
     // 处理rules
@@ -49,6 +81,14 @@ module.exports = function (source) {
             console.warn(`the rule '${key} is not a function`)
         }
     }
+
+    //处理文本中{}
+    const textFn = md.renderer.rules.text
+    md.renderer.rules.text = function () {
+        const ret = textFn.apply(this, arguments)
+        return ret.replace(/[\{\}]/g, (match) => `{'${match}'}`)
+    }
+
     // 处理plugins
     for (let plugin of plugins) {
         if (Array.isArray(plugin)) {
